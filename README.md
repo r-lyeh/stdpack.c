75c5dbad2a214964ee504d276e62b12044395977
r-lyeh <rlyeh.2015@gmail.com>
2019-12-31 02:14:30 +0100

    Add LZP1

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 This is a collection of public domain compressors from different authors. Sources have been minimally refurbished where due and/or backported from C++ to plain C. Most modifications were adding a standarized api convention and converting into single-header, converting from file to in-memory based, also added re-entrancy and thread-safety fixes, portability improvements, etc.
 
-At this point, you do not likely want to use these compressors in your serious products. First-citizen compressors like Snappy, LZ4/LZ4HC, ZSTD, ZLIB, BZIP2 or LZMA SDK, are much more mature, more portable, extremely faster and way more battle-tested codebases than the ones found here. Anyways, if your main motivations are (like mine) having more liberal licensing terms, smaller codebases, ease of free standalone functions, or love header-only files or think they produce less hassles in general, etc. then you are welcome here.
+At this point, you do not likely want to use these compressors in serious projects. First-citizen compressors like Snappy, LZ4/LZ4HC, ZSTD, ZLIB, BZIP2 or LZMA SDK are generally better bets: way more faster, mature, portable and battle-tested than the snippets found here. Still, you are welcome to use these snippets if your motivations are similar to mine: to have less headaches by embedding 3rd party software with no clauses, by preferring header-only files, by not hand-crafting makefiles, by avoid dealing with dynamic libraries, and by inspecting and debugging small codebases in general.
 
-By using these compressors you are not required to credit original authors neither to display licensing terms in your end-products, but I encourage you to acknowledge the original authors if possible, as they crafted with <3 these little gems over the years.
+By using these compressors you are not required to credit original authors neither to display licensing terms in your final products, but I encourage you to acknowledge the original authors if possible, as they crafted with <3 these little gems over the years.
 
 # About compression (tips)
 
@@ -16,18 +16,19 @@ By using these compressors you are not required to credit original authors neith
 
 1. Finally, consider transcoding to faster codecs if you plan to reload same data over and over. Ie, during your game installation you may transcode offline LZMA streams and save them as LZ4X for faster runtime loading.
 
-1. TL;DR? Personal preferences? balz:1 for offline tasks, ulz:0 or lz4x:1 for runtime tasks, zlib:3 elsewhere.
+1. TL;DR? Personal preferences? balz:1 for offline tasks, ulz:0 or lz4x:1 for runtime tasks, defl:3 elsewhere.
 
 # Some metrics (YMMV)
 
 ```
 C:\prj\stdarc>demo.exe --0 \prj\enwik8
 Y 100000000 ->    58769363 ppp  58.77% c:0.783s d:0.495s \prj\enwik8
+Y 100000000 ->    56089203 lzp1 56.09% c:0.926s d:0.637s \prj\enwik8
 Y 100000000 ->    49063365 lzss 49.06% c:14.17s d:0.424s \prj\enwik8
 Y 100000000 ->    47873720 lzw3 47.87% c:1.143s d:0.372s \prj\enwik8
 Y 100000000 ->    52660722 lz4x 52.66% c:1.004s d:0.165s \prj\enwik8
 Y 100000000 ->    51940556 ulz  51.94% c:0.690s d:0.176s \prj\enwik8
-Y 100000000 ->    63192823 zlib 63.19% c:1.837s d:0.541s \prj\enwik8
+Y 100000000 ->    63192823 defl 63.19% c:1.837s d:0.541s \prj\enwik8
 Y 100000000 ->    47639504 crsh 47.64% c:2.098s d:0.530s \prj\enwik8
 Y 100000000 ->    30253237 balz 30.25% c:11.07s d:5.253s \prj\enwik8
 Y 100000000 ->    39973598 lzma 39.97% c:13.65s d:2.810s \prj\enwik8
@@ -37,11 +38,12 @@ Y 100000000 ->    39973598 lzma 39.97% c:13.65s d:2.810s \prj\enwik8
 ```
 C:\prj\stdarc>demo.exe --10 \prj\enwik8
 Y 100000000 ->    58769363 ppp  58.77% c:0.783s d:0.474s \prj\enwik8
+Y 100000000 ->    56089203 lzp1 56.09% c:0.926s d:0.637s \prj\enwik8
 Y 100000000 ->    49063365 lzss 49.06% c:13.76s d:0.417s \prj\enwik8
 Y 100000000 ->    47873720 lzw3 47.87% c:1.164s d:0.367s \prj\enwik8
 Y 100000000 ->    45876420 lz4x 45.88% c:1.459s d:0.166s \prj\enwik8
 Y 100000000 ->    40285775 ulz  40.29% c:11.95s d:0.169s \prj\enwik8
-Y 100000000 ->    36465845 zlib 36.47% c:6.581s d:0.462s \prj\enwik8
+Y 100000000 ->    36465845 defl 36.47% c:6.581s d:0.462s \prj\enwik8
 Y 100000000 ->    31856827 crsh 31.86% c:333.2s d:0.419s \prj\enwik8
 Y 100000000 ->    28460117 balz 28.46% c:32.36s d:4.989s \prj\enwik8
 Y 100000000 ->    26702913 lzma 26.70% c:86.37s d:1.577s \prj\enwik8
@@ -62,5 +64,5 @@ Y 100000000 ->    26702913 lzma 26.70% c:86.37s d:1.577s \prj\enwik8
 - Add vfs and mounting support. Maybe.
 - Optimize LZSS. Could compressor be nearly as performant as LZJB with some effort?
 - Optimize BALZ. Could those in-mem bufferings be skipped?
-- Consider ZPAQ (large C++), BCM (divsufsort is MIT licensed) and yalz77 too. LZP1, LZJB maybe?
+- Consider ZPAQ (large C++), BCM (divsufsort is MIT licensed) and yalz77 too. LZJB maybe?
 - Bugfixes and cleaning up.
---
+++ lzp1.c
@@ -0,0 +1,340 @@
+/***********
+
+Direct port of the old lzp1.c code to a single file header.
+
+This is not the best way to make fast compressors on modern hardware
+and this is by no means a modern competitive compressor.
+
+Also, zlib licensed is not strictly public domain, but pretty close terms :o)
+
+-----------
+
+Copyright (c) 2019, @r-lyeh
+Copyright (c) 1998-2012, Charles Bloom
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+   1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+   2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+   3. This notice may not be removed or altered from any source
+   distribution.
+
+*******************/
+
+unsigned lzp1_encode(const void* in, unsigned inlen, void* out, unsigned outlen, unsigned flags);
+unsigned lzp1_decode(const void* in, unsigned inlen, void* out, unsigned outlen);
+
+
+#ifdef LZP1_C
+#pragma once
+
+#define LZP1_BOUNDS(sz)    ((sz)+((sz)/8)+256)
+#define LZP1_EXCESS        256
+
+#define LZP1_HASH_SIZE  (1<<16)
+#define LZP1_HASH(x,y,z) ((x ^ (y << 7) ^ (z<<11)) & 0xFFFF)
+
+static int lzp1_encode_(const uint8_t *raw,int rawLen,uint8_t * comp,int compLen)
+{
+    uint8_t const *table[LZP1_HASH_SIZE];
+    for(int ix=0;ix<LZP1_HASH_SIZE;ix++) table[ix] = raw;
+
+    uint8_t *cp,*controlp;
+    const uint8_t *rp,*endrp,*mp;
+    int ix,control,controlb,ml;
+    uint8_t literal;
+
+    /** do the LZP **/
+
+    rp = raw;   endrp = raw + rawLen;
+    cp = comp;
+
+    // store excess
+    *cp++ = rawLen & 255;
+
+    // seed four
+    *cp++ = *rp++; *cp++ = *rp++; *cp++ = *rp++; *cp++ = *rp++;
+    
+    control = 0; controlp = cp++; controlb = 8;
+
+/** the control-byte entry macro **/
+
+#define ENC_SHIFT_CONTROL(bit)  if ( 0 ) ; else { control += control + bit; if ( --controlb == 0 ) {    *controlp = (uint8_t)control;   controlp    = cp++; control     = 0;    controlb    = 8;    } }
+
+    while(rp < endrp) {
+
+        ix = LZP1_HASH(rp[-1],rp[-2],rp[-3]);
+        mp = table[ix]; table[ix] = rp;
+
+        if ( *mp != *rp ) {
+            literal = *rp++;
+
+            ix = LZP1_HASH(rp[-1],rp[-2],rp[-3]);
+            mp = table[ix]; table[ix] = rp;
+
+            if ( *mp != *rp ) {
+                ENC_SHIFT_CONTROL(0);   //flag two literals : 0
+                *cp++ = literal;
+                *cp++ = *rp++;  // pass a literal
+            } else {
+                ENC_SHIFT_CONTROL(1);   //flag literal then a match : 10
+                ENC_SHIFT_CONTROL(0);
+                *cp++ = literal;
+                goto encode_match;
+            }
+        } else {
+            ENC_SHIFT_CONTROL(1);   //flag a match with no literals : 11
+            ENC_SHIFT_CONTROL(1);
+
+        encode_match:
+
+            mp++; rp++;
+
+            if ( *mp != *rp ) {
+                ENC_SHIFT_CONTROL(0);
+            } else {
+                mp++; rp++;
+                ENC_SHIFT_CONTROL(1);       
+
+                if ( *mp == *rp ) { mp++; rp++;
+                    if ( *mp == *rp ) { mp++; rp++;
+                        if ( *mp == *rp ) { mp++; rp++;
+                            // flag more than 3
+                            ENC_SHIFT_CONTROL(1);
+                            ENC_SHIFT_CONTROL(1);
+                            if ( *mp == *rp ) { mp++; rp++;
+                                if ( *mp == *rp ) { mp++; rp++;
+                                    if ( *mp == *rp ) { mp++; rp++;
+                                        if ( *mp == *rp ) { mp++; rp++;
+                                            if ( *mp == *rp ) { mp++; rp++;
+                                                if ( *mp == *rp ) { mp++; rp++;
+                                                    if ( *mp == *rp ) { mp++; rp++;
+                                                            // flag 11 or more
+                                                            ENC_SHIFT_CONTROL(1);
+                                                            ENC_SHIFT_CONTROL(1);
+                                                            ENC_SHIFT_CONTROL(1);
+
+                                                            ml = 0;
+                                                            while( *mp == *rp ) {
+                                                                mp++; rp++; ml++;
+                                                            }
+                                                            while( ml >= 0xFF ) {
+                                                                *cp++ = 0xFF; ml -= 0xFF;
+                                                            }
+                                                            *cp++ = (uint8_t)ml;
+
+                                                    }   else {  // match 10
+                                                        ENC_SHIFT_CONTROL(1);
+                                                        ENC_SHIFT_CONTROL(1);
+                                                        ENC_SHIFT_CONTROL(0);
+                                                    }
+                                                } else {    // match 9
+                                                    ENC_SHIFT_CONTROL(1);
+                                                    ENC_SHIFT_CONTROL(0);
+                                                    ENC_SHIFT_CONTROL(1);
+                                                }
+                                            } else {    // match 8
+                                                ENC_SHIFT_CONTROL(1);
+                                                ENC_SHIFT_CONTROL(0);
+                                                ENC_SHIFT_CONTROL(0);
+                                            }
+                                        } else {    // match 7
+                                            ENC_SHIFT_CONTROL(0);
+                                            ENC_SHIFT_CONTROL(1);
+                                            ENC_SHIFT_CONTROL(1);
+                                        }
+                                    } else {    // match 6
+                                        ENC_SHIFT_CONTROL(0);
+                                        ENC_SHIFT_CONTROL(1);
+                                        ENC_SHIFT_CONTROL(0);
+                                    }
+                                } else {    // match 5
+                                    ENC_SHIFT_CONTROL(0);
+                                    ENC_SHIFT_CONTROL(0);
+                                    ENC_SHIFT_CONTROL(1);
+                                }
+                            }   else {  // match 4
+                                ENC_SHIFT_CONTROL(0);
+                                ENC_SHIFT_CONTROL(0);
+                                ENC_SHIFT_CONTROL(0);
+                            }
+                        } else {    // match 3
+                            ENC_SHIFT_CONTROL(1);
+                            ENC_SHIFT_CONTROL(0);
+                        }
+                    } else {    // match 2
+                        ENC_SHIFT_CONTROL(0);
+                        ENC_SHIFT_CONTROL(1);
+                    }
+                } else {    //match 1
+                    ENC_SHIFT_CONTROL(0);
+                    ENC_SHIFT_CONTROL(0);
+                }
+            }
+        }
+    }
+
+    //flush the control
+
+    while( controlb > 0 ) {
+        control += control;
+        controlb--;
+    }
+    *controlp = (uint8_t)control;
+
+    return (int)(cp - comp);
+}
+
+static int lzp1_decode_(const uint8_t * comp,int compLen,uint8_t * raw,int rawLen)
+{
+    uint8_t const *table[LZP1_HASH_SIZE];
+    for(int ix=0;ix<LZP1_HASH_SIZE;ix++) table[ix] = raw;
+
+    const uint8_t *cp,*mp,*endcp;
+    uint8_t *rp,*endrp;
+    int ix,control,controlb,ml;
+    int bit;
+
+    rp = raw;   endrp = raw + rawLen;
+    cp = comp;  endcp = comp + compLen;
+
+    uint8_t excess = *cp++; compLen--;
+
+    *rp++ = *cp++; *rp++ = *cp++; *rp++ = *cp++; *rp++ = *cp++;
+
+    control = *cp++;
+    controlb = 8;
+
+#define DEC_GET_CONTROL(getbit) if ( 0 ) ; else { getbit = control & 0x80;  control += control;if ( --controlb == 0 ) { control = *cp++;    controlb = 8; } }
+
+    while(cp<endcp) {
+
+        DEC_GET_CONTROL(bit);
+        if ( ! bit ) {  // two literals
+            table[ LZP1_HASH(rp[-1],rp[-2],rp[-3]) ] = rp;  *rp++ = *cp++;
+            table[ LZP1_HASH(rp[-1],rp[-2],rp[-3]) ] = rp;  *rp++ = *cp++;
+
+        } else {
+            DEC_GET_CONTROL(bit);
+            if ( ! bit ) {  //10 : literal then match
+                table[ LZP1_HASH(rp[-1],rp[-2],rp[-3]) ] = rp;  *rp++ = *cp++;
+            }   
+
+            // match
+
+            ix = LZP1_HASH(rp[-1],rp[-2],rp[-3]);   mp = table[ix]; table[ix] = rp;
+
+            *rp++ = *mp++;
+
+            // read 1 bit
+            DEC_GET_CONTROL(bit);
+            if ( bit ) {
+                *rp++ = *mp++;
+                // read 2 bits to get length
+                DEC_GET_CONTROL(bit);
+                if ( bit ) {
+                    *rp++ = *mp++; *rp++ = *mp++;
+                    DEC_GET_CONTROL(bit);
+                    if ( bit ) {
+                        *rp++ = *mp++;
+                        //read 3 more bits
+
+                        DEC_GET_CONTROL(bit);   if ( bit ) {
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                            DEC_GET_CONTROL(bit);   if ( bit ) {
+                                DEC_GET_CONTROL(bit);   if ( bit ) {    // 111
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+
+                                    do {
+                                        int l;
+                                        l = ml = *cp++;
+                                        while(l--)
+                                            *rp++ = *mp++;
+                                    } while( ml == 0xFF );
+
+                                } else {                                                        // 110
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                                }
+                            } else {
+                                DEC_GET_CONTROL(bit);   if ( bit ) {    // 101
+                                    *rp++ = *mp++;
+                                } else {                                                        // 100
+                                }
+                            }
+                        } else {
+                            DEC_GET_CONTROL(bit);   if ( bit ) {
+                                DEC_GET_CONTROL(bit);   if ( bit ) {    // 011
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                                } else {                                                        // 010
+                                    *rp++ = *mp++;
+                                    *rp++ = *mp++;
+                                }
+                            } else {
+                                DEC_GET_CONTROL(bit);   if ( bit ) {    // 001
+                                    *rp++ = *mp++;
+                                } else {                                                        // 000
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    DEC_GET_CONTROL(bit);
+                    if ( bit ) {
+                        *rp++ = *mp++;
+                    }
+                }
+            }
+        }
+    }
+
+    return (((int)(rp - raw) >> 8) << 8) | excess;
+}
+
+
+unsigned lzp1_encode(const void* in, unsigned inlen, void* out, unsigned outlen, unsigned flags) {
+    return (unsigned)lzp1_encode_((const uint8_t*)in, (int)inlen, (uint8_t*)out, (int)outlen);
+}
+unsigned lzp1_decode(const void* in, unsigned inlen, void* out, unsigned outlen) {
+    return (unsigned)lzp1_decode_((const uint8_t*)in, (int)inlen, (uint8_t*)out, (int)outlen);
+}
+
+#endif // LZP1_C
+
+
+#ifdef LZP1_DEMO
+#pragma once
+int main(int argc, char** argv) {
+    const char *longcopy = "Hello world! Hello world! Hello world! Hello world!";
+
+    char out[128];
+    int outlen = lzp1_encode(longcopy, strlen(longcopy)+1, out, 128);
+    printf("%s %d->%d\n", outlen ? "ok" : "fail", (int)strlen(longcopy)+1, outlen);
+
+    char redo[128 + 256];
+    int unpacked = lzp1_decode(out, outlen, redo, 128);
+    printf("%d->%d %s\n", outlen, unpacked, redo);
+}
+#define main main__
+#endif // LZP1_DEMO
+
+
